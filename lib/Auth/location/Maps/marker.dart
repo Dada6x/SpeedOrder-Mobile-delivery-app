5b911d84e2321470ec8e21f6c08fdbe2afe2dd5c
Dada <dada.66.6x.outlook@gmail.com>
2024-12-21 22:09:34 +0300

    removed the other class (nothing changes )

--- lib/Auth/location/Maps/marker.dart
+++
@@ -1,8 +0,0 @@
-import 'package:latlong2/latlong.dart';
-
-class MarkerData {
-  final LatLng? position;
-  final String? title;
-  final String? description;
-  MarkerData({this.position, this.title, this.description});
-}
