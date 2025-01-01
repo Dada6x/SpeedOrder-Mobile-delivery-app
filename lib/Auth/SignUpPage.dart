60c478f2d6db6802c7b5e251758add0cf74e3827
Dada <dada.66.6x.outlook@gmail.com>
2025-01-01 16:03:26 +0300

    made seperate image picker

--- lib/Auth/SignUpPage.dart
+++ lib/Auth/SignUpPage.dart
@@ -8,6 +8,7 @@ import 'package:mamamia_uniproject/Location/setLocation.dart';
 import 'package:mamamia_uniproject/components/Button.dart';
 import 'package:mamamia_uniproject/components/normal_appbar.dart';
 import 'package:mamamia_uniproject/Auth/model.dart';
+import 'package:mamamia_uniproject/image_picker/image_picker.dart';
 import 'package:mamamia_uniproject/main.dart';
 
 class SignupPage extends StatefulWidget {
@@ -192,65 +193,3 @@ InputDecoration inputDecoration(BuildContext context,
 
 //! yahea : image picker needs to be in independent class
 
-class ImagePickingDialog extends StatefulWidget {
-  const ImagePickingDialog({super.key});
-
-  @override
-  State<ImagePickingDialog> createState() => _ImagePickingDialogState();
-}
-
-class _ImagePickingDialogState extends State<ImagePickingDialog> {
-//! Close the dialog after image is selected
-  Future pickImageFromGallery() async {
-    final returnedImage =
-        await ImagePicker().pickImage(source: ImageSource.gallery);
-    if (returnedImage == null) return;
-    _selectedImage = File(returnedImage.path);
-    giveselectedImage(_selectedImage!);
-    Navigator.pop(context);
-  }
-
-  Future pickImageFromCamera() async {
-    final returnedImage =
-        await ImagePicker().pickImage(source: ImageSource.camera);
-    if (returnedImage == null) return;
-
-    _selectedImage = File(returnedImage.path);
-    giveselectedImage(_selectedImage!);
-    Navigator.pop(context);
-  }
-
-  void giveselectedImage(File img) {
-    Get.find<Model>().changeImage(img);
-  }
-
-  File? _selectedImage;
-  @override
-  Widget build(BuildContext context) {
-    return GetBuilder(
-        init: Model(),
-        builder: (controller) => Dialog(
-              child: SizedBox(
-                width: controller.screenWidth(context) * 0.5,
-                height: controller.screenHeight(context) * 0.25,
-                child: SimpleDialog(
-                  contentPadding: const EdgeInsets.all(0),
-                  children: [
-                    ListTile(
-                      iconColor: Theme.of(context).colorScheme.primary,
-                      leading: const Icon(FontAwesomeIcons.image),
-                      title: const Text("Gallery Image"),
-                      onTap: pickImageFromGallery,
-                    ),
-                    ListTile(
-                      iconColor: Theme.of(context).colorScheme.primary,
-                      leading: const Icon(FontAwesomeIcons.camera),
-                      title: const Text("Camera Image"),
-                      onTap: pickImageFromCamera,
-                    ),
-                  ],
-                ),
-              ),
-            ));
-  }
-}
