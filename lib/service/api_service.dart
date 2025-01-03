d033115ae41ab2342c35b728c99917cb03d84a08
Zain00F <clasheyad@gmail.com>
2025-01-03 16:52:30 +0300

    MInor changes

---
+++ lib/service/api_service.dart
@@ -0,0 +1,14 @@
+// import 'package:flutter/material.dart';
+
+// class ApiService extends StatelessWidget {
+//   // const ApiService({super.key});
+//   final String apiHead =
+//       "http://127.0.0.1:8000/api/register?name=mohammed&password=1234567890&user_phone=0987654321";
+//   final String apiRequest = "register";
+//   final String api = "$ h";
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Placeholder();
+//   }
+// }
